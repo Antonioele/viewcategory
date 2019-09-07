b8a48ae0b7bd33e9dae914368bc2fc5a7d69e5ae
antonioele <antonio.ele@tiscali.it>
2019-09-07 17:23:40 +0200

    style for med

--- packages/venia-ui/lib/components/viewcategory/bcat.js
+++ packages/venia-ui/lib/components/viewcategory/bcat.js
@@ -4,10 +4,27 @@ import Trigger from '../Trigger';
 import Icon from '../Icon';
 import { Menu as MenuIcon, Search as SearchIcon } from 'react-feather';
 import { Meh  } from 'react-feather';
+import { AlignCenter } from 'react-feather';
 
 
   const Bcat = () =>
   {
+    const divStyle = {
+      width: '50px',
+      margin: '100px'
+     
+    };
+
+    const pStyle = {
+      
+      
+      
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+        AlignCenter: 'center',
+      margin : 'auto'
+    };
     
     const [random, setRandom] = useState(Math.random());
     const reRender = () => setRandom(Math.random());
@@ -20,12 +37,12 @@ import { Meh  } from 'react-feather';
   
   const antonio="antonio";
      return(
-       <div>
+       <div style={divStyle} >
          
          
          <Trigger action={reRender}>
      
-     <Icon src={Meh} />
+     <Icon style={{justiftyContent:"center", alignItems:"center"}} src={Meh} />
 </Trigger>
      </div>
      );
