3fd4930f27de264e4936a51ce6d86f6478d31511
antonioele <antonio.ele@tiscali.it>
2019-09-07 19:01:24 +0200

    create follow event panel white

--- packages/venia-ui/lib/components/viewcategory/bcat.js
+++ packages/venia-ui/lib/components/viewcategory/bcat.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Trigger from '../Trigger';
 import Icon from '../Icon';
 import { Menu as MenuIcon, Search as SearchIcon } from 'react-feather';
 import { Meh  } from 'react-feather';
 import { AlignCenter } from 'react-feather';
-
+import defaultClasses from './bcat.css';
 
   const Bcat = () =>
   {
@@ -17,8 +17,6 @@ import { AlignCenter } from 'react-feather';
 
     const pStyle = {
       
-      
-      
       position: 'absolute',
       left: '50%',
       top: '50%',
@@ -26,25 +24,31 @@ import { AlignCenter } from 'react-feather';
       margin : 'auto'
     };
     
-    const [random, setRandom] = useState(Math.random());
-    const reRender = () => setRandom(Math.random());
-    
+    const [random, setRandom] = useState(0);
+    const reRender = () => setRandom(1);
+    const rootClassName = random ? defaultClasses.root_open : defaultClasses.root;
     useEffect(() => {
       
       // Update the document title using the browser API
       
     });
   
-  const antonio="antonio";
+  
      return(
+      <React.Fragment> 
        <div style={divStyle} >
          
-         
+         <h1>{random}</h1>
          <Trigger action={reRender}>
      
      <Icon style={{justiftyContent:"center", alignItems:"center"}} src={Meh} />
 </Trigger>
      </div>
+     <div>
+     <aside className={rootClassName}>  plutoo</aside>
+
+     </div>
+     </React.Fragment>
      );
        }
  
